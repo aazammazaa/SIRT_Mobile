a929925e3a3a4e3feb1fea5e321c94e3946a5e2b
syed_tanveer <syedtanveerabid@live.com>
2017-04-06 20:27:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sirt/sirtmobile/ParentActivity.java
@@ -0,0 +1,10 @@
+package com.example.sirt.sirtmobile;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tanveer on 06-Apr-17.
+ */
+
+public class ParentActivity extends AppCompatActivity {
+}
