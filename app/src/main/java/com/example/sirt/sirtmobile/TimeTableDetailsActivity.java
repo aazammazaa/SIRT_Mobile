e7f865866384e521ecf5e8d400526de4e6a69951
syed_tanveer <syedtanveerabid@live.com>
2017-05-23 16:45:16 +0530

    Add default tab selection by Day of the week.

--- app/src/main/java/com/example/sirt/sirtmobile/TimeTableDetailsActivity.java
+++ app/src/main/java/com/example/sirt/sirtmobile/TimeTableDetailsActivity.java
@@ -5,23 +5,22 @@ package com.example.sirt.sirtmobile;
  */
 
 import android.content.Intent;
-import android.icu.text.SimpleDateFormat;
-//import android.icu.util.Calendar;
-import android.icu.util.*;
-//import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TabHost;
-import java.util.Calendar;
+
+import java.util.*;
+
 
 public class TimeTableDetailsActivity extends TimeTableSectionActivity {
     int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Calendar calendar = Calendar.getInstance();
+        TimeZone timeZone = TimeZone.getTimeZone("IST");
+        Calendar calendar = Calendar.getInstance(timeZone);
         int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 
         Intent i = getIntent();
@@ -30,6 +29,7 @@ public class TimeTableDetailsActivity extends TimeTableSectionActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.time_table_detail_main);
+
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = (ViewPager) findViewById(R.id.time_table_viewpager);
 
@@ -42,6 +42,11 @@ public class TimeTableDetailsActivity extends TimeTableSectionActivity {
         tabLayout.setupWithViewPager(viewPager);
 
         //Set default tab
-        viewPager.setCurrentItem(dayOfWeek);
+        if (dayOfWeek == 3) viewPager.setCurrentItem(1);
+        else if (dayOfWeek == 4) viewPager.setCurrentItem(2);
+        else if (dayOfWeek == 5) viewPager.setCurrentItem(3);
+        else if (dayOfWeek == 6) viewPager.setCurrentItem(4);
+        else if (dayOfWeek == 2) viewPager.setCurrentItem(0);
+
     }
 }
