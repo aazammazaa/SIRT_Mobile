80374463995e65fd2cda386c5865e2a30795fc91
vinimishra <vinimishra1996@gmail.com>
2017-05-24 12:17:08 +0530

    Done Wednesday Fragment

--- app/src/main/java/com/example/sirt/sirtmobile/NewsActivity.java
+++ app/src/main/java/com/example/sirt/sirtmobile/NewsActivity.java
@@ -16,10 +16,10 @@ public class NewsActivity extends ParentActivity {
         setContentView(R.layout.activity_news);
 
         final ArrayList<MainAdapter> words = new ArrayList<MainAdapter>();
-        words.add(new MainAdapter(R.drawable.newspaper, "VI Semester Registration", "09/02/2017 registration of students"));
-        words.add(new MainAdapter(R.drawable.newspaper, "SAGAR UTSAV", "Coming Soon"));
-        words.add(new MainAdapter(R.drawable.newspaper, "MID-SEM", "Dates will be announced soon"));
-        words.add(new MainAdapter(R.drawable.newspaper, "SAGAR UTSAV", "Food Fun"));
+        words.add(new MainAdapter(R.drawable.newspaper, "VI Semester Registration", "Hello, students!\n" +
+                "All the students of the 6th semester are requested to come with their student id cards, mark sheets of previous semesters and positively get registered on 09/02/2017. After 09/02/2017 per day penalty will be added to the student's profile."));
+        words.add(new MainAdapter(R.drawable.newspaper, "SAGAR UTSAV COMING SOON", "The wait is over now. Sagar Utsav 2k17 is coming soon with new themes, new fun. Just be ready to enjoy. "));
+        words.add(new MainAdapter(R.drawable.newspaper, "SAGAR UTSAV DRESS CODE", "Sagar Utsav 2k17 is coming with dress themes and lots of new fun. Dress theme for Day 1: Red, Black, Blue, Green, and Yellow. Dress theme for Day 2: White for girls and Blue for boys. Dress theme for Day3: Traditional."));
         words.add(new MainAdapter(R.drawable.newspaper, "SAGAR UTSAV", "DAY1: Wall Painting, Ramp Walk, Dance And Singing"));
         words.add(new MainAdapter(R.drawable.newspaper, "SAGAR UTSAV", "DAY2: Treasure Hunt, Sports"));
         words.add(new MainAdapter(R.drawable.newspaper, "SAGAR UTSAV", "DAY3: Traditional Day, Prize Distribution"));
--- app/src/main/java/com/example/sirt/sirtmobile/TimeTableWednesdayFragment.java
+++ app/src/main/java/com/example/sirt/sirtmobile/TimeTableWednesdayFragment.java
@@ -27,65 +27,65 @@ public class TimeTableWednesdayFragment extends Fragment {
         switch (position) {
             case 0: //IV A
                 words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", "ADC", "Mr. Manohar Joshi"));
-                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", " ", " "));
+                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", "TNP Lecture - II", "Mr. Mahendra Joshi"));
+                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", "ADA", "Mr. Deepak Choudhary"));
+                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", "Material Sc.", "Mr. Himanshu Nautiyal"));
                 words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", "CSO", "Dr. Vipin Tiwari"));
-                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", " ", " "));
+                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", "SE", "Ms. Priyanka Saxena"));
+                words.add(new TimeTableDetailsWord("02:10 PM", "03:00 PM", "TOC", "Mr. Arun Jhapate"));
                 break;
             case 1: //IV B
-                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", " ", " "));
+                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", "ADC", "Mr. Veerendra Singh"));
+                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", "SE", "Mr. Rahul Jain"));
+                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", "TNP Lecture - II", "Mr. Mahendra Joshi"));
+                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", "TOC", "Mr. Rajesh K Shukla"));
+                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", "CSO", "Mr. Mayank Namdeo"));
+                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", "Material Sc.", "Mr. Himanshu Nautiyal"));
+                words.add(new TimeTableDetailsWord("02:10 PM", "03:00 PM", "ADA", "Mr. Sandeep Waderkar"));
                 break;
             case 2: //IV C
-                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", " ", " "));
+                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", "Material SC.", "Mr. Ankur Shrivastava"));
+                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", "ADC/ADA Lab", "Mr. Virendara Singh\nMr. Mayank Namdeo"));
+                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", "ADC/ADA Lab", "Mr. Virendara Singh\nMr. Mayank Namdeo"));
+                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", "TNP Lecture - II", "Mr. Mahendra Joshi"));
+                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", "TOC", "Mr. Rajesh K Shukla"));
+                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", "CSO", "Mr. Dheeresh Soni"));
+                words.add(new TimeTableDetailsWord("02:10 PM", "03:00 PM", "Library & GD/SS", " "));
                 break;
             case 3: //VI A
-                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", " ", " "));
+                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", "SEPM", "Ms. Priyanka Saxena"));
+                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", "Microprocessor", "Ms. Sangeeta Shukla"));
+                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", "PPL", "Ms. Sanyogita Soni"));
+                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", "CN", "Mr. Sandeep Waderkar"));
+                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", "CN/Microprocessor Lab", "Mr. Sandeep Waderkar \nMs. Sngeeta Shukla"));
+                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", "CN/Microprocessor Lab", "Mr. Sandeep Waderkar \nMs. Sngeeta Shukla"));
+                words.add(new TimeTableDetailsWord("02:10 PM", "03:00 PM", "ACA", "Ms. Mohini Parihar"));
                 break;
             case 4: //VI B
-                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", " ", " "));
+                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", "Microprocessor/SEPM Lab", "Ms. Sngeeta Shukla\nMs.Anjana Nigam"));
+                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", "Microprocessor/SEPM Lab", "Ms. Sngeeta Shukla\nMs.Anjana Nigam"));
+                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", "SEPM", "Dr. M Kumar"));
+                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", "Microprocessor", "Ms. Sngeeta Shukla"));
+                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", "CN", "Ms. Ruchi Thakur"));
+                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", "ACA", "Ms. Anjana Nigam"));
+                words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", "PPL", "Ms. Sanyogita Soni"));
                 break;
             case 5: //VI C
-                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", " ", " "));
-                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", " ", " "));
-                words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", " ", " "));
+                words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", "Microprocessor", "Ms. Sngeeta Shukla"));
+                words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", "SEPM", "Mr. Sunil Malviya"));
+                words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", "CN", "Mr.Arun Jhapate"));
+                words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", "ACA", "Ms. Shalu Arya"));
+                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", "Microprocessor/CN Lab", "Ms. Sangeeta Shukla\nMr. Arun Jhapate"));
+                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", "Microprocessor/CN Lab", "Ms. Sangeeta Shukla \nMr. Arun Jhapate"));
+                words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", "PPL", "Dr. Vipin Tiwari"));
                 break;
             case 6: //VIII A
                 words.add(new TimeTableDetailsWord("08:30 AM", "09:20 AM", "Soft Comp.", " Mr. Arun Jhapate"));
                 words.add(new TimeTableDetailsWord("09:20 AM", "10:10 AM", "Web Engg.", "Mr. Pawan Sharma"));
                 words.add(new TimeTableDetailsWord("10:10 AM", "11:00 AM", "N/W & Web Sec ", "Mr. Ganesh Patidar"));
                 words.add(new TimeTableDetailsWord("11:00 AM", "11:50 AM", "Wireless", "Dr. Rashmi Soni"));
-                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", "Soft Comp. Batch II \nWeb Engg. Batch I", "Mr. Arun Jhapate\nMr. Pawan Sharma"));
-                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", "Soft Comp. Batch II \nWeb Engg. Batch I", "Mr. Arun Jhapate\nMr. Pawan Sharma"));
+                words.add(new TimeTableDetailsWord("12:30 PM", "01:20 PM", "Soft Comp. /Web Engg. Lab", "Mr. Arun Jhapate\nMr. Pawan Sharma"));
+                words.add(new TimeTableDetailsWord("01:20 PM", "02:10 PM", "Soft Comp. /Web Engg. Lab", "Mr. Arun Jhapate\nMr. Pawan Sharma"));
                 words.add(new TimeTableDetailsWord("02:10 PM", "11:00 PM", "Counselling", " "));
                 break;
         }
